62ba2a3bd2b82db33d95d4e576bc1486c3108db1
Oleh Basystyi <basystyo@gmail.com>
2025-11-02 15:43:44 +0200

    fix: change default value of conv's padding to None

--- src/nn/conv.py
+++ src/nn/conv.py
@@ -9,7 +9,7 @@ def autopad(k, p=None, d=1):
     return p
 
 class Conv(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, padding=1, groups=1, dilation=1, act=True):
+    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, padding=None, groups=1, dilation=1, act=True):
         super(Conv, self).__init__()
         self.conv = nn.Conv2d(
             in_channels, out_channels, kernel_size, stride,
