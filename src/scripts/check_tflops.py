8a38a368201e2fa900d999ad2c1b364e5452b5b2
Oleh Basystyi <basystyo@gmail.com>
2025-11-05 12:27:35 +0200

    chore: format code

--- src/scripts/check_tflops.py
+++ src/scripts/check_tflops.py
@@ -4,9 +4,11 @@ from calflops import calculate_flops
 model = YOLO("yolov8n.pt")
 batch_size = 16
 input_shape = (batch_size, 3, 640, 640)
-flops, macs, params = calculate_flops(model=model, 
-                                      input_shape=input_shape,
-                                      output_as_string=True,
-                                      output_precision=4,
-                                      print_detailed=True)
+flops, macs, params = calculate_flops(
+    model=model,
+    input_shape=input_shape,
+    output_as_string=True,
+    output_precision=4,
+    print_detailed=True,
+)
 # print("YOLOv8 FLOPs:%s   MACs:%s   Params:%s \n" %(flops, macs, params))
--- src/utils/random.py
+++ src/utils/random.py
@@ -2,6 +2,7 @@ import random
 import numpy as np
 import torch
 
+
 def set_seed(seed: int, deterministic: bool = False):
     """
     Set the random seed for reproducibility across random, numpy, and torch.
